751fe149e29cf4e523ff0ec9cce5f4eb49df9a3b
Arkadii Kozhuhow <arkadii_kozhuhow@mail.ru>
2022-07-25 21:09:02 +0700

    add async-requests -hw

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,15 @@
+document.forms.form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const xhr = new XMLHttpRequest();
+	document.getElementById('send').onclick = function() {
+		xhr.addEventListener('readystatechange', () => {
+			if (xhr.readyState === xhr.DONE && xhr.status == 200) {
+				xhr.upload.onprogress = function(event) {
+					document.getElementById('progress').value = match.round(((event.loaded * 100) / event.total) / 100);
+				}
+			}			
+		})
+	}
+	xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+	xhr.send(document.getElementById('file').files[0]);
+});
